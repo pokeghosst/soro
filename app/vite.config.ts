606bb98099750d9841fa7b3fa055b73b4167bea1
pokeghost <pokeghost@disroot.org>
2025-11-15 12:33:27 +0200

    chore(DX): Re-initialize Vue project and set up pnpm workspace

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -6,11 +6,13 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
-  plugins: [vue(), vueDevTools()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
 })
